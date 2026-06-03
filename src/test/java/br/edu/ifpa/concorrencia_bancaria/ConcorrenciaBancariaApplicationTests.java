4f6a3cfa888f7b5ef9760267c0cf84077f3cc19b
Laura Alves <lauraalves.contato@gmail.com>
2026-06-03 15:42:22 -0300

    Init spring boot project

---
+++ src/test/java/br/edu/ifpa/concorrencia_bancaria/ConcorrenciaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpa.concorrencia_bancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcorrenciaBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
